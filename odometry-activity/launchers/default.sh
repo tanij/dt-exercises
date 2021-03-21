1012e3e62b39f3cefb7dead229aa00525f6d9fd5
Vincenzo <polivicio@gmail.com>
2021-03-21 14:02:25 +0100

    Odometry activity in the simulator running dts exercises test --sim. Need notebooks converter.

---
+++ odometry-activity/launchers/default.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source /environment.sh
+
+# initialize launch file
+dt-launchfile-init
+
+# YOUR CODE BELOW THIS LINE
+# ----------------------------------------------------------------------------
+
+# NOTE: Use the variable DT_REPO_PATH to know the absolute path to your code
+# NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
+
+# launching app
+
+source /code/exercise_ws/devel/setup.bash
+python3 /code/solution.py &
+roslaunch --wait car_interface all.launch veh:=$VEHICLE_NAME &
+
+
+dt-exec roslaunch encoder_pose encoder_pose_node.launch veh:=$VEHICLE_NAME
+
+
+# ----------------------------------------------------------------------------
+# YOUR CODE ABOVE THIS LINE
+
+# wait for app to end
+dt-launchfile-join
