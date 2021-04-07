19bcb5b01503807c4823e7fc3447bd69437a8ada
Andrea Censi <acensi@ethz.ch>
2021-04-07 23:55:40 +0200

    added lane follwoing

--- template_ros/launchers/run_all.sh
+++ template_ros/launchers/run_all.sh
@@ -4,5 +4,10 @@ source /opt/ros/noetic/setup.bash
 source /code/catkin_ws/devel/setup.bash
 source /code/exercise_ws/devel/setup.bash
 python3 /code/solution.py &
+echo "Launching car_interface"
 roslaunch --wait car_interface all.launch veh:=$VEHICLE_NAME &
-roslaunch --wait duckietown_demos lane_following.launch
+echo "Launching duckietown_demos"
+roslaunch --wait duckietown_demos lane_following.launch &
+sleep 5
+echo "Switching to LANE_FOLLOWING"
+rostopic pub /$VEHICLE_NAME/fsm_node/mode duckietown_msgs/FSMState '{header: {}, state: "LANE_FOLLOWING"}'
